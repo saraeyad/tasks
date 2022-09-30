eca6039956f3cbec350a6344ce4a2105eac129b2
saraeyad <saraehismail@gmail.com>
2022-09-30 17:47:56 +0300

    update

---
+++ sets1.py
@@ -0,0 +1,7 @@
+sample_set = {"Yellow", "Orange", "Black"}
+sample_list = ["Blue", "Green", "Red"]
+
+sample_set.update(sample_list)
+
+print("set : ")
+print(sample_set)
---
+++ tuple1.py
@@ -0,0 +1,3 @@
+tuple1 = (10, 20, 30, 40, 50)
+tuple1 = tuple(reversed(tuple1))
+print(tuple1)
